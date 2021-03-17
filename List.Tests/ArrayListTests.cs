981b7e96f10a2f53cd814586ef8f7ec82b78509c
Nebo <kindersmail08@gmail.com>
2021-03-17 13:32:42 +0300

    add tests by 5 methods

--- List.Tests/ArrayListTests.cs
+++ List.Tests/ArrayListTests.cs
@@ -8,13 +8,70 @@ namespace List.Tests
 {
     class ArrayListTests
     {
-        [TestCase()]
-        public void Test1(int value)
+        [TestCase(15, new int[] { 30, 60, 90, 120, 150, 180, 15})]
+        public void Test1(int a, int[] expectedArray)
         {
-            ArrayList expected = new ArrayList();
-            ArrayList actual = new ArrayList();
-
+            ArrayList expected = new ArrayList(expectedArray);
+            ArrayList actual = new ArrayList(new int[] { 30, 60, 90, 120, 150, 180 });
+            actual.Add(a);
             Assert.AreEqual(expected, actual);
         }
+
+        [TestCase(15, new int[] { 15, 30, 60, 90, 120, 150, 180})]
+        public void Test2(int a, int[] expectedArray)
+        {
+            ArrayList expected = new ArrayList(expectedArray);
+            ArrayList actual = new ArrayList(new int[] { 30, 60, 90, 120, 150, 180 });
+            actual.AddToTheBeginning(a);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        //[TestCase(15, 3, new int[] { 30, 60, 90, 15, 120, 150, 180 })]
+        //public void Test3(int a, int index, int[] expectedArray)
+        //{
+        //    ArrayList expected = new ArrayList(expectedArray);
+        //    ArrayList actual = new ArrayList(new int[] { 30, 60, 90, 120, 150, 180 });
+        //    actual.AddByIndex(a, index);
+        //    Assert.AreEqual(expected, actual);
+        //}
+
+
+        //[TestCase(new int[] { 30, 60, 90, 120, 150})]
+        //public void Test4(int[] expectedArray)
+        //{
+        //    ArrayList expected = new ArrayList(expectedArray);
+        //    ArrayList actual = new ArrayList(new int[] { 30, 60, 90, 120, 150, 180 });
+        //    actual.RemoveValueFromTheEnd();
+        //    Assert.AreEqual(expected, actual);
+        //}
+
+
+        //[TestCase(new int[] { 60, 90, 120, 150, 180 })]
+        //public void Test5(int[] expectedArray)
+        //{
+        //    ArrayList expected = new ArrayList(expectedArray);
+        //    ArrayList actual = new ArrayList(new int[] { 30, 60, 90, 120, 150, 180 });
+        //    actual.RemoveValueFromTheBeginning();
+        //    Assert.AreEqual(expected, actual);
+        //}
+
+
+        //[TestCase(2, new int[] { 30, 60, 120, 150, 180 })]
+        //public void Test6(int index, int[] expectedArray)
+        //{
+        //    ArrayList expected = new ArrayList(expectedArray);
+        //    ArrayList actual = new ArrayList(new int[] { 30, 60, 90, 120, 150, 180 });
+        //    actual.RemoveValueByIndex(index);
+        //    Assert.AreEqual(expected, actual);
+        //}
+
+
+
+
+
+
     }
+
+
 }
